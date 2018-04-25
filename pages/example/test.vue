228219426a540c4fdd6ca719b47bb23aa8f42852
HanPil Park <hanpilpark@wemakeprice.com>
2018-04-25 16:57:01 +0900

    cart example

--- pages/example/test.vue
+++ pages/example/test.vue
@@ -7,19 +7,6 @@
 <script>
 
 export default {
-  data () {
-    return {
-      title: '안녕 세계2'
-    }
-  },
-  head () {
-    return {
-      title: this.title,
-      meta: [
-        { hid: 'test', name: '설명', content: '내가 커스텀한 설명' }
-      ]
-    }
-  },
   async asyncData ( { store, params, error, req }) {
     await store.dispatch('getIntData', { url: '/api/cart', type: ''});
   }
--- pages/index.vue
+++ pages/index.vue
@@ -7,19 +7,6 @@
 <script>
 
 export default {
-  data () {
-    return {
-      title: '안녕 세계!'
-    }
-  },
-  head () {
-    return {
-      title: this.title,
-      meta: [
-        { hid: 'index', name: '설명', content: '내가 커스텀한 설명' }
-      ]
-    }
-  },
   async asyncData ( { store, params, error, req }) {
     await store.dispatch('getIntData', { url: '/api', type: ''});
   }
