36754002f6bcdddfdfba4e45ebaddbac5f4cf6ea
HanPil Park <hanpilpark@wemakeprice.com>
2018-04-25 10:57:12 +0900

    cart example

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,16 @@
 <template>
   <section>
-    <v-container fluid grid-list-lg>
-      <v-subheader>{{ $store.state.initialData.product.title }}</v-subheader>
-      <v-layout row wrap>
-        <v-flex xs12 sm6 md4 v-for="(list, index) in $store.state.initialData.product.list" :key="index">
-          <app-card v-bind:list="list"/>
-        </v-flex>
-      </v-layout>
-    </v-container>
+    <div>INdex</div>
   </section>
 </template>
 
 <script>
-import AppCard from '@/components/Card'
 
 export default {
   components: { AppCard },
   async asyncData ( { store, params, error, req }) {
-    await store.dispatch('getIntData', { url: '/api/cart', type: ''});
+    await store.dispatch('getIntData', { url: '/api', type: ''});
   }
 }
 </script>
 
-// export default {
-
-// }
-
-<style>
-.red {
-  color: red;
-}
-</style>
